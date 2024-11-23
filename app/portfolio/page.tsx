785a978c135d4d8f2c9eceb964f5c5a52de41d07
Koonosuke <kou@cps.im.dendai.ac.jp>
2024-11-23 12:02:04 +0900

    画像コメント

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,11 +1,16 @@
 // portfolio/page.tsx
+import { Suspense } from "react";
 import PortfolioCard from "../components/PortfolioCard";
 
 export default function PortfolioPage() {
   return (
     <section className="py-10">
-      <h2 className="text-2xl font-bold mb-4">Portfolio</h2>
-      <PortfolioCard />
+      <h2 className="text-2xl font-bold mb-4">
+        Portfolio→画像をタップして詳細へ
+      </h2>
+      <Suspense>
+        <PortfolioCard />
+      </Suspense>
     </section>
   );
 }
